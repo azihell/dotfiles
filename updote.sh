5d4b51f522f50129e7d25dcd44b8966f14215c6b
Aziel Freitas <azielfreitas@gmail.com>
2022-01-02 00:16:35 -0300

    Fix script for fish shell

--- updote.sh
+++ updote.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/fish
 
 echo "Running rsync in dry-run mode"
-rsync -vr --dry-run --files-from=dotfiles_list $HOME $(pwd)
+rsync -vr --dry-run --files-from=dotfiles_list $HOME (pwd)
